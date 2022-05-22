a4829f94511b7601689c29da7ca597b685d3af04
serinko <johanesxsalmon@riseup.net>
2022-05-22 09:29:42 +0200

    cli-sketch

--- gki_monitor.py
+++ gki_monitor.py
@@ -16,7 +16,7 @@ kt_prt = ''
 # OPTIONS IN CLI:
 # 1 Calculate GKI
 # 2 Calculate Glucose
-# 3 Calculate Ketose
+# 3 Calculate Ketones
 # 4 See past data
 
 # Second level branch
@@ -26,6 +26,10 @@ kt_prt = ''
 # 2 Is your glusoces in
 # 2.1 mg/dL
 # 2.2 or in mmol/L
+# $ What is the time preference you want to see:
+# 4 Do you want to see a graph
+# 4.1 Yes
+# 4.2 No table is enough
 
 met = input(met_prt)
 
@@ -34,7 +38,7 @@ if met == '1':
     kt = float(input(kt_prt))
 elif met == '2':
     gl = float(input(gl_prt))
-    
+
 # GKI CALCULATOR
 # What do we measure gl and kt in?
 gki_equation = ''
